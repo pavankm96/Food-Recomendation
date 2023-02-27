73d285972c4db2d67659ff06dedb4f893effd8a8
Pavan K M <33823728+pavankm96@users.noreply.github.com>
2023-02-27 13:39:56 +0530

    Update stream_mob.py

--- stream_mob.py
+++ stream_mob.py
@@ -94,7 +94,7 @@ def main():
                 #rec_log.write(str(user_id))
                 #rec_log.close()
                 #subprocess.run(["streamlit", "run", "food_det_mob.py"])
-                doc_ref = db.collection(u'users').document(u'{}'.format(recent_login))
+                doc_ref = db.collection(u'users').document(u'{}'.format('recent_login'))
                 doc_ref.set({u'recent_login':u'{}'.format(user_id)})
                 link="https://pavankm96-food-recomendation-food-det-mob-ou4oy3.streamlit.app/"
                 st.markdown(link, unsafe_allow_html=True)
